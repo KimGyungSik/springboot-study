5c23adc5be24cba46b90a2318709cb7677c271f4
KimGyungSik <0711kyungh@naver.com>
2024-07-16 15:15:40 +0900

    first commit

---
+++ src/main/java/com/fastcampus/springboot_study/SpringbootStudyApplication.java
@@ -0,0 +1,21 @@
+package com.fastcampus.springboot_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringbootStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootStudyApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "hello springboot 222233";
+    }
+
+}
